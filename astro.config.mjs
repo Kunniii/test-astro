4c6efa830f190f027c859ddd7764428d78477e23
Nguyen Phi Truong <nguyenairport56@gmail.com>
2022-11-29 13:52:17 +0700

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://kunniii.github.io/test-astro',
 	integrations: [mdx(), sitemap()],
 });
